34de1117d0a2cf467404e1eddfc0fc85de135cf4
ChoiDongJoon <adong0808@naver.com>
2024-11-07 00:06:31 +0900

    feat(convenience-store): PromotionType 필드 추가, Getter 추가
    PromotionType 필드 추가, Getter 추가

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,17 +1,49 @@
 package store.domain;
 
+import store.enumerate.ProductType;
+
 public class Product {
 
     private String name;
     private int price;
     private int quantity;
     private String promotion;
+    private ProductType productType;
 
     public Product(String name, int price, int quantity, String promotion) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
-        this.promotion = promotion;
+        this.promotion = isPromotionNull(promotion);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getPromotion() {
+        return promotion;
+    }
+
+    public boolean isPromotion() {
+        return productType.equals(ProductType.PROMOTION);
+    }
+
+    private String isPromotionNull(String promotion) {
+        if (promotion.equals("null")) {
+            this.productType = ProductType.REGULAR;
+            return "";
+        }
+        this.productType = ProductType.PROMOTION;
+        return promotion;
     }
 
     @Override
@@ -21,6 +53,9 @@ public class Product {
                 ", price=" + price +
                 ", quantity=" + quantity +
                 ", promotion='" + promotion + '\'' +
+                ", productType=" + productType +
                 '}';
     }
+
+    //"null"이 아니라 null일 경우 예외처리
 }
