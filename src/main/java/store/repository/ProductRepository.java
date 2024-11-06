883a43caf71b2dfdcdb3c0e73ef57ab39b3a6241
ChoiDongJoon <adong0808@naver.com>
2024-11-07 02:58:33 +0900

    feat(convenience-store): 프로모션 상품 리스트와 일반 상품 리스트를 매번 조회해서 최신화되도록 변경
    프로모션 상품 리스트와 일반 상품 리스트를 매번 조회해서 최신화되도록 변경

--- src/main/java/store/repository/ProductRepository.java
+++ src/main/java/store/repository/ProductRepository.java
@@ -2,55 +2,51 @@ package store.repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import store.domain.Product;
 
 public class ProductRepository {
 
     private final List<Product> products = new ArrayList<>();
-    List<Product> promotionProducts = new ArrayList<>();
-    List<Product> regularProducts = new ArrayList<>();
 
     public void addProduct(Product product) {
         products.add(product);
-    }
-
-    public List<Product> getPromotionProducts() {
-        for (Product product : products) {
-            if (product.isPromotion()) {
-                promotionProducts.add(product);
-            }
-        }
-        return promotionProducts;
-    }
-
-    public List<Product> getRegularProducts() {
-        for (Product product : products) {
-            if (!product.isPromotion()) {
-                regularProducts.add(product);
-            }
-        }
-        return regularProducts;
     }
 
     public List<Product> getAllProducts() {
         return products;
     }
 
+    public List<Product> getPromotionProducts() {
+        return products.stream()
+                .filter(Product::isPromotion)
+                .collect(Collectors.toList());
+    }
+
+    public List<Product> getRegularProducts() {
+        return products.stream()
+                .filter(product -> !product.isPromotion())
+                .collect(Collectors.toList());
+    }
+
     public Product getPromotionProductByName(String name) {
-        return promotionProducts.stream()
+        return getPromotionProducts().stream()
                 .filter(product -> product.getName().equals(name))
                 .findFirst()
                 .orElse(null);
     }
 
     public Product getRegularProductByName(String name) {
-        return regularProducts.stream()
+        return getRegularProducts().stream()
                 .filter(product -> product.getName().equals(name))
                 .findFirst()
                 .orElse(null);
     }
 
-    public void decreaseProductQuantity(Product product) {
-
+    public void decreasePromotionProductQuantity(String name, int amount) {
+        Product promotionProduct = getPromotionProductByName(name);
+        if (promotionProduct != null) {
+            promotionProduct.decreaseQuantity(amount);
+        }
     }
 }
