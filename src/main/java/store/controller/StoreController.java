a152f8f3038cb4227d633af1291601a1aeb23f39
ChoiDongJoon <adong0808@naver.com>
2024-11-07 02:59:41 +0900

    feat(convenience-store): 프로모션 적용 구현
    프로모션 적용 구현

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,9 +1,19 @@
 package store.controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.extension.ExtensionContext.Store;
 import store.domain.Product;
 import store.domain.Promotion;
+import store.dto.PromotionApplyResult;
+import store.service.ProductService;
+import store.service.PromotionService;
 import store.view.InputView;
 import store.view.OutputView;
 
@@ -11,18 +21,63 @@ public class StoreController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final ProductService productService;
+    private final PromotionService promotionService;
 
-    public StoreController(InputView inputView, OutputView outputView) {
+    public StoreController(InputView inputView, OutputView outputView, ProductService productService, PromotionService promotionService) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.productService = productService;
+        this.promotionService = promotionService;
     }
 
     public void startProcess() {
-        List<Product> productList = inputView.getProductList();
-        List<Promotion> promotionList = inputView.getPromotionList();
+        List<Product> productList = productService.getAllProducts();
+        List<Promotion> promotionList = promotionService.getAllPromotions();
         outputView.printWelcomeAndStockList(productList);
 
-        List<String> buyProductAmount = inputView.getBuyProductAmount();
+        List<Product> buyProducts = productService.getBuyProductAmount();
+        List<Product> buyProductsCopy = buyProducts.stream()
+                .map(Product::clone)
+                .collect(Collectors.toList());
+        Map<String, Promotion> activePromotions = findActivePromotionsForBuyProducts(buyProducts);
+
+        Map<Product, PromotionApplyResult> productPromotionApplyResults = applyPromotions(buyProducts, activePromotions);
+
+        outputView.printReceipt(productList, buyProductsCopy, productPromotionApplyResults);
+    }
+
+    private Map<Product, PromotionApplyResult> applyPromotions(List<Product> buyProducts, Map<String, Promotion> activePromotions) {
+        Map<Product, PromotionApplyResult> applyResult = new HashMap<>();
+        for (Product buyProduct : buyProducts) {
+            if(activePromotions.containsKey(buyProduct.getName())) {
+                Promotion promotion = activePromotions.get(buyProduct.getName());
+                int promotionBuyAmount = promotion.getBuyAmount();
+                int promotionGetAmount = promotion.getGetAmount();
+                int promotionTotalAmount = promotionBuyAmount + promotionGetAmount;
+
+                Product promotionProduct = productService.getPromotionProductByName(buyProduct.getName());
+                int totalGetAmount = 0;
+                int totalPromotedPrice = 0;
+
+                while(buyProduct.getQuantity() >= promotionBuyAmount && promotionProduct.getQuantity() >= promotionTotalAmount){
+                    totalGetAmount += promotionGetAmount;
+                    totalPromotedPrice += promotionBuyAmount * promotionProduct.getPrice();
+                    buyProduct.decreaseQuantity(promotionTotalAmount);
+                    promotionProduct.decreaseQuantity(promotionTotalAmount);
+                }
+                applyResult.put(buyProduct, new PromotionApplyResult(totalGetAmount, totalPromotedPrice));
+            }
+        }
+        return applyResult;
+    }
+
+    private Map<String ,Promotion> findActivePromotionsForBuyProducts(List<Product> buyProducts) {
+        Map<String ,Promotion> activePromotions = new HashMap<>();
+
+        for (Product buyProduct : buyProducts) {
+            String name = buyProduct.getName();
+            Product promotionProduct = productService.getPromotionProductByName(name);
 
             if(promotionProduct != null) {
                 String promotion = promotionProduct.getPromotion();
