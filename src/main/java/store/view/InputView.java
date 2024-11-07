0b5ca26ecac7f86f83d75201033247d7b7d86de0
ChoiDongJoon <adong0808@naver.com>
2024-11-07 13:49:41 +0900

    refactor(convenience-store): 추가 구매 의사 묻기 추가
    추가 구매 의사 묻기 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -8,7 +8,7 @@ import store.service.PromotionService;
 public class InputView {
 
     private static final String MEMBERSHIP_DISCOUNT_QUERY_MESSAGE = "멤버십 할인을 받으시겠습니까? (Y/N)";
-
+    private static final String ADDITIONAL_PURCHASE_QUERY_MESSAGE = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
 
     public String getBuyProductAmount() {
         return Console.readLine();
@@ -16,6 +16,11 @@ public class InputView {
 
     public String checkMembership() {
         System.out.println(MEMBERSHIP_DISCOUNT_QUERY_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String checkAddtionalPurchase() {
+        System.out.println(ADDITIONAL_PURCHASE_QUERY_MESSAGE);
         return Console.readLine();
     }
 
