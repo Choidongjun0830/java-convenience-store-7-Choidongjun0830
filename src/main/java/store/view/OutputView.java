15fd45cec48575ec052aa172bc26bf44d7700952
ChoiDongJoon <adong0808@naver.com>
2024-11-08 17:57:21 +0900

    refactor(convenience-store): 기능별로 함수 분리
    기능별로 함수 분리

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,10 +1,9 @@
 package store.view;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import store.domain.Product;
 import store.dto.PromotionApplyResult;
+import store.dto.ReceiptInfo;
 import store.util.MoneyFormatter;
 
 public class OutputView {
@@ -14,12 +13,13 @@ public class OutputView {
     private static final String PRODUCT_INPUT_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
 
     private static final String RECEIPT_HEADER = "===========W 편의점=============";
-    private static final String BUY_PRODUCT_LIST_HEADER = "상품명\t\t수량\t금액";
-    private static final String GET_PRODUCT_LIST_HEADER = "===========증\t정=============";
+    private static final String PURCHASE_PRODUCT_LIST_HEADER = "상품명\t\t수량\t금액";
+    private static final String GET_PROMOTION_PRODUCT_LIST_HEADER = "===========증\t정=============";
     private static final String PRICE_LIST_HEADER = "==============================";
 
     private static final String PROMOTION_NOT_APPLICABLE_MESSAGE = "프로모션 적용이 가능한 상품에 대해 고객이 해당 수량만큼 가져오지 않았을 경우, 안내에 대한 메시지 출력";
     private static final String PROMOTION_SHORTAGE_MESSAGE = "프로모션 재고가 부족하여 일부 수량을 프로모션 없이 구매가 결정되어야 하면, 일부 수량에 대해 정가로 결제되었으며 이에 대한 안내 메시지 출력";
+    private static final String STOCK_SHORTAGE = "재고 없음";
 
     private final MoneyFormatter moneyFormatter;
 
@@ -44,45 +44,51 @@ public class OutputView {
 
     public void printReceipt(List<Product> productList,
                              List<Product> buyProducts,
-                             Map<Product, PromotionApplyResult> productPromotionApplyResults,
-                             int totalProductPrice,
-                             int membershipSaleAmount) {
+                             List<PromotionApplyResult> productPromotionApplyResults,
+                             ReceiptInfo receiptInfo) {
         System.out.println(RECEIPT_HEADER);
-        System.out.println(BUY_PRODUCT_LIST_HEADER);
-        int totalAllProductPrice = 0;
+        int totalAllProductQuantity = printPurchaseProducts(productList, buyProducts);
+
+        printPromotionProducts(productPromotionApplyResults);
+        int totalPromotedPrice = getTotalPromotedPrice(productPromotionApplyResults);
+        printTotalPurchaseResult(receiptInfo.getTotalProductPrice(), receiptInfo.getMembershipSaleAmount(), totalAllProductQuantity, totalPromotedPrice);
+    }
+
+
+    private int printPurchaseProducts(List<Product> productList, List<Product> buyProducts) {
+        System.out.println(PURCHASE_PRODUCT_LIST_HEADER);
         int totalAllProductQuantity = 0;
         for (Product buyProduct : buyProducts) {
             int totalPrice = getProductPrice(buyProduct.getName(), productList) * buyProduct.getQuantity();
-            totalAllProductPrice += totalPrice;
             int quantity = buyProduct.getQuantity();
             totalAllProductQuantity += quantity;
             System.out.println(buyProduct.getName() + "\t\t" + quantity + "\t" + MoneyFormatter.formatMoney(totalPrice));
         }
-        System.out.println(GET_PRODUCT_LIST_HEADER);
-        for (Entry<Product, PromotionApplyResult> productPromotionApplyResult : productPromotionApplyResults.entrySet()) {
-            Product promotionProduct = productPromotionApplyResult.getKey();
-            PromotionApplyResult promotionApplyResult = productPromotionApplyResult.getValue();
-            System.out.println(promotionProduct.getName() + "\t" + promotionApplyResult.getTotalGetAmount());
+        return totalAllProductQuantity;
+    }
+
+    private static void printPromotionProducts(List<PromotionApplyResult> productPromotionApplyResults) {
+        System.out.println(GET_PROMOTION_PRODUCT_LIST_HEADER);
+        for (PromotionApplyResult productPromotionApplyResult : productPromotionApplyResults) {
+            Product promotionProduct = productPromotionApplyResult.getProduct();
+            System.out.println(promotionProduct.getName() + "\t" + productPromotionApplyResult.getTotalGetAmount());
         }
-        int totalPromotedPrice = getTotalPromotedPrice(productPromotionApplyResults);
+    }
+
+    private static void printTotalPurchaseResult(int totalProductPrice, int membershipSaleAmount, int totalAllProductQuantity,
+                                                 int totalPromotedPrice) {
         System.out.println(PRICE_LIST_HEADER);
-        System.out.println("총구매액" + "\t" + totalAllProductQuantity + "\t" + MoneyFormatter.formatMoney(totalProductPrice));
+        System.out.println("총구매액" + "\t" + totalAllProductQuantity + "\t" + MoneyFormatter.formatMoney(
+                totalProductPrice));
         System.out.println("행사할인" + "\t" + MoneyFormatter.formatMoney(totalPromotedPrice));
         System.out.println("멤버십할인" + "\t" + MoneyFormatter.formatMoney(membershipSaleAmount));
-        System.out.println("내실돈" + "\t" + MoneyFormatter.formatMoney(totalProductPrice + totalPromotedPrice + membershipSaleAmount));
-    }
-
-    public void printPromotionNotApplicable() {
-        System.out.println(PROMOTION_NOT_APPLICABLE_MESSAGE);
-    }
-
-    public void printPromotionShortage() {
-        System.out.println(PROMOTION_SHORTAGE_MESSAGE);
+        System.out.println("내실돈" + "\t" + MoneyFormatter.formatMoney(
+                totalProductPrice + totalPromotedPrice + membershipSaleAmount));
     }
 
     private String isQuantityZero(int quantity) {
         if (quantity == 0) {
-            return "재고 없음";
+            return "STOCK_SHORTAGE";
         }
         return String.valueOf(quantity) + "개";
     }
@@ -96,9 +102,9 @@ public class OutputView {
         return 0;
     }
 
-    private int getTotalPromotedPrice(Map<Product, PromotionApplyResult> productPromotionApplyResults) {
+    private int getTotalPromotedPrice(List<PromotionApplyResult> productPromotionApplyResults) {
         int totalPromotedPrice = 0;
-        for (PromotionApplyResult promotionApplyResult : productPromotionApplyResults.values()) {
+        for (PromotionApplyResult promotionApplyResult : productPromotionApplyResults) {
             totalPromotedPrice += promotionApplyResult.getTotalPromotedSalePrice();
         }
         return -totalPromotedPrice;
