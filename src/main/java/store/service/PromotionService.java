25cba236d383b1c65423dfdc05741792d07f4aae
ChoiDongJoon <adong0808@naver.com>
2024-11-08 18:10:29 +0900

    refactor(convenience-store): Promotion관련 비즈니스 로직 PromotionService로 이동
    Promotion관련 비즈니스 로직 PromotionService로 이동

--- src/main/java/store/service/PromotionService.java
+++ src/main/java/store/service/PromotionService.java
@@ -1,20 +1,29 @@
 package store.service;
 
+import camp.nextstep.edu.missionutils.DateTimes;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import store.domain.Product;
 import store.domain.Promotion;
 import store.repository.PromotionRepository;
+import store.view.InputView;
 
 public class PromotionService {
 
     private final PromotionRepository promotionRepository;
+    private final ProductService productService;
+    private final InputView inputView;
 
-    public PromotionService(PromotionRepository promotionRepository) {
+    public PromotionService(PromotionRepository promotionRepository, ProductService productService, InputView inputView) {
         this.promotionRepository = promotionRepository;
+        this.productService = productService;
+        this.inputView = inputView;
     }
 
     private void parseProducts(String filePath) {
@@ -36,14 +45,70 @@ public class PromotionService {
         }
     }
 
-    public List<Promotion> getAllPromotions() {
+    public void getAllPromotions() {
         if(promotionRepository.getAllPromotions().isEmpty()) {
             parseProducts("src/main/resources/promotions.md");
         }
-        return promotionRepository.getAllPromotions();
     }
 
     public Promotion getPromotionByName(String name) {
         return promotionRepository.getPromotionByName(name);
+    }
+
+    public Map<String, Promotion> findActivePromotions(List<Product> buyProducts) {
+        Map<String, Promotion> activePromotions = new HashMap<>();
+        for (Product buyProduct : buyProducts) {
+            String name = buyProduct.getName();
+            Product promotionProduct = productService.getPromotionProductByName(name);
+
+            if (promotionProduct != null) {
+                String promotion = promotionProduct.getPromotion();
+                Promotion activePromotion = getActivePromotion(promotion);
+                if (activePromotion != null) { //지워도 될듯
+                    activePromotions.put(name, activePromotion);
+                }
+            }
+        }
+        return activePromotions;
+    }
+
+    private Promotion getActivePromotion(String promotionName) {
+        Promotion promotion = getPromotionByName(promotionName);
+        if (promotion != null && isPromotionActive(promotion)) {
+            return promotion;
+        }
+        return null;
+    }
+
+    private boolean isPromotionActive(Promotion promotion) {
+        LocalDate now = LocalDate.from(DateTimes.now());
+        return !now.isBefore(promotion.getStartDate()) && !now.isAfter(promotion.getEndDate());
+    }
+
+    public boolean applyExtraForPromo(Product buyProduct, List<Product> buyProductClone, int promotionBuyAmount,
+                                       int promotionTotalAmount, int promotionGetAmount) {
+        if(buyProduct.getQuantity() == promotionBuyAmount && buyProduct.getQuantity() < promotionTotalAmount) {;
+            String response = inputView.checkAdditionalQuantity(buyProduct.getName(), promotionGetAmount);
+            if(response.equalsIgnoreCase("Y")) {
+                buyProduct.increaseQuantity(promotionGetAmount);
+                productService.increaseTotalPurchaseAmount(buyProduct.getName(), buyProductClone, promotionGetAmount);
+            }
+            if(buyProduct.getQuantity() < promotionGetAmount) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int checkPurchaseWithoutPromotion(Product buyProduct) {
+        if(buyProduct.getQuantity() > 0) {
+            String checkPurchaseWithoutPromotion = inputView.checkPurchaseWithoutPromotion(buyProduct.getName(), buyProduct.getQuantity());
+            if(checkPurchaseWithoutPromotion.equals("N")) {
+                int result = buyProduct.getQuantity();
+                buyProduct.decreaseQuantity(buyProduct.getQuantity());
+                return result;
+            }
+        }
+        return 0;
     }
 }
