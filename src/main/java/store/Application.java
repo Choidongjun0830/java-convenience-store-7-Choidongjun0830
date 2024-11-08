dfdebf08ef39bfc7f7a16b924a90b128e5289c1b
ChoiDongJoon <adong0808@naver.com>
2024-11-09 03:38:09 +0900

    feat(convenience-store): main에 실행 셋업, storeController.startProcess() 호출
    main에 실행 셋업, storeController.startProcess() 호출

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,33 @@
 package store;
 
+import store.controller.StoreController;
+import store.repository.ProductRepository;
+import store.repository.PromotionRepository;
+import store.service.ProductService;
+import store.service.PromotionService;
+import store.service.StoreService;
+import store.util.MoneyFormatter;
+import store.validator.InputValidator;
+import store.view.InputView;
+import store.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        ProductRepository productRepository = new ProductRepository();
+        PromotionRepository promotionRepository = new PromotionRepository();
+        MoneyFormatter moneyFormatter = new MoneyFormatter();
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView(moneyFormatter);
+        InputValidator inputValidator = new InputValidator();
+
+        ProductService productService = new ProductService(productRepository, inputView);
+        PromotionService promotionService = new PromotionService(promotionRepository, productService, inputView, inputValidator);
+        StoreService storeService = new StoreService(productService, promotionService);
+
+
+        StoreController storeController = new StoreController(inputView, outputView, productService, promotionService, storeService, inputValidator);
+        storeController.startProcess();
     }
 }
