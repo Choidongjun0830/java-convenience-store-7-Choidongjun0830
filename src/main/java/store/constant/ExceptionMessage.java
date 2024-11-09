1033f5b44a417ff3c1d5eaa906d7a7e0d8846331
ChoiDongJoon <adong0808@naver.com>
2024-11-09 22:31:55 +0900

    refactor(convenience-store): 예외 메시지 수정
    예외 메시지 수정

--- src/main/java/store/constant/ExceptionMessage.java
+++ src/main/java/store/constant/ExceptionMessage.java
@@ -7,7 +7,5 @@ public class ExceptionMessage {
     public static final String INVALID_FORMAT_EXCEPTION = EXCEPTION_PREFIX + " 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.";
     public static final String NOT_EXIST_PRODUCT_EXCEPTION = EXCEPTION_PREFIX + " 존재하지 않는 상품입니다. 다시 입력해 주세요.";
     public static final String INVALID_INPUT_EXCEPTION = EXCEPTION_PREFIX + " 잘못된 입력입니다. 다시 입력해 주세요.";
-    public static final String PURCHASE_NAME_BLANK_EXCEPTION = EXCEPTION_PREFIX + " 상품 이름은 공백될 수 없습니다.";
-    public static final String PURCHASE_AMOUNT_INVALID_EXCEPTION = EXCEPTION_PREFIX + " 구매할 상품 수량은 1보다 작을 수 없습니다.";
-    public static final String PURCHASE_PRODUCTS_DELIMITER_EXCEPTION = EXCEPTION_PREFIX + " '-' 구분자를 사용해주세요.";
+    public static final String PURCHASE_AMOUNT_INVALID_EXCEPTION = EXCEPTION_PREFIX + " 구매할 상품 수량은 1보다 작을 수 없습니다. 다시 입력해 주세요.";
 }
