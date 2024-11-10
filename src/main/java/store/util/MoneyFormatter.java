11f74a902c5a20cfaf7349b1794ee6d89a62c123
ChoiDongJoon <adong0808@naver.com>
2024-11-10 15:43:01 +0900

    feat(convenience-store): 0일 경우 - 붙이기
    0일 경우 - 붙이기

--- src/main/java/store/util/MoneyFormatter.java
+++ src/main/java/store/util/MoneyFormatter.java
@@ -3,6 +3,9 @@ package store.util;
 public class MoneyFormatter {
 
     public static String formatMoney(int price) {
+        if(price == 0) {
+            return "-" + price;
+        }
         return String.format("%,d", price);
     }
 }
