1d1430824d849a684033693f6dde03e476d1903e
ChoiDongJoon <adong0808@naver.com>
2024-11-10 15:57:17 +0900

    feat(convenience-store): -0 적용 제거
    -0 적용 제거

--- src/main/java/store/util/MoneyFormatter.java
+++ src/main/java/store/util/MoneyFormatter.java
@@ -3,9 +3,6 @@ package store.util;
 public class MoneyFormatter {
 
     public static String formatMoney(int price) {
-        if(price == 0) {
-            return "-" + price;
-        }
         return String.format("%,d", price);
     }
 }
